8e67dc3a8d44377780496c068e7159dd24f90b60
nikolatrajkovicITS <nikolatrajkovic.contact@gmail.com>
2021-02-20 14:13:15 +0100

    Integrated esbuild-wsam service

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,39 @@
+import * as esbuild from 'esbuild-wasm';
 import ReactDOM from 'react-dom';
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
 
 const App = () => {
+  const serviceRef = useRef<any>()
   const [input, setInput] = useState('');
   const [code, setCode] = useState('');
 
-  const onClick = () => {
-    console.log(input)
+
+  const startService = async () => {
+    // service gonna use for bundle and transpile code
+    serviceRef.current = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm'
+    })
+
   }
+
+  useEffect(() => {
+    startService();
+  }, [])
+
+  const onClick = async () => {
+    if (!serviceRef.current) {
+      return;
+    }
+
+    const result = await serviceRef.current.transform(input, {
+      loader: 'jsx',
+      target: 'es2015'
+    });
+
+    setCode(result.code)
+  }
+
 
   return (
     <div>
